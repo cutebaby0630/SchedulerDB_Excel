cf968ebea2233b81909e30d707a2c2d447783b3a
cutebaby0630 <lovemath0630@gmail.com>
2020-07-15 16:16:00 +0800

    one sheet data import

--- SchedulerDB/Program.cs
+++ SchedulerDB/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Immutable;
 using System.ComponentModel.Design;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography.Pkcs;
@@ -44,52 +45,52 @@ namespace SchedulerDB
     }
     class Program
     {
-      
+
         static void Main(string[] args)
         {
-            string sql = @"IF object_id('tempdb..#RESTTReservation') IS NOT NULL DROP TABLE #RESTTReservation
+             string sql = @"IF object_id('tempdb..#RESTTReservation') IS NOT NULL DROP TABLE #RESTTReservation
 
-                           SELECT a.RoomCode RESRoomCode,
-                                  b.RoomCode XRYRoomCode,a.CalendarGroupName,
-                                  a.MedicalNoteNo,a.ExaRequestNo,a.[Start],b.PlanDate ,a.ReservationSourceType,a.SourceCode,
-                                  b.DVC_CHRT,b.DVC_RQNO,b.DVC_DATE,b.DVC_STTM,b.SourceCode XRYSourceCode
-                           INTO #RESTTReservation
-                           FROM 
-                           (
-                             SELECT d.RoomCode,d.CalendarGroupName,a.MedicalNoteNo,a.ExaRequestNo,c.[Start],a.ReservationSourceType,
-                                    'RESTTReservation' SourceCode
-                             FROM HISSCHDB.dbo.RESTTReservation a
-                             INNER JOIN HISSCHDB.dbo.RESTTimeslotRes b ON a.Id = b.ReservationId
-                             INNER JOIN HISSCHDB.dbo.RESTTimeslot c ON b.TimeslotId = c.Id
-                             LEFT JOIN HISSCHDB.dbo.tmpEXAMRoomMapping d ON a.CalendarId = d.CalendarId
-                             WHERE c.[Start] >= convert(date,dateadd(day,-30,getdate()))
-                             --ORDER BY d.RoomCode,c.[Start]
-                           ) a
-                           FULL OUTER JOIN 
-                           (
-                            SELECT SUBSTRING(a.DVC_ROOM,2,2) RoomCode,a.DVC_CHRT,a.DVC_RQNO,
-                           
-                           CASE WHEN ISNUMERIC(DVC_DATE) = 1 AND ISNUMERIC(DVC_STTM) = 1 
-                           THEN try_convert(datetime,convert(varchar, substring((
-                                           (CASE WHEN LEN(rtrim(DVC_DATE)) = 7 
-                                                 THEN DVC_DATE 
-                            ELSE (CASE WHEN LEFT(DVC_DATE,1) = '0' THEN '1' ELSE '0' END) + DVC_DATE END )) ,1,3)+ 1911)+RIGHT(DVC_DATE,4) +' '+substring(DVC_STTM,1,2)+':'+substring(DVC_STTM,3,2))     
-                            WHEN ISNUMERIC(DVC_DATE) = 1 
-                            THEN try_convert(date,convert(varchar, substring((
-                            (CASE WHEN LEN(rtrim(DVC_DATE)) = 7 
-                                  THEN DVC_DATE 
-                               ELSE (CASE WHEN LEFT(DVC_DATE,1) = '0' THEN '1' ELSE '0' END) + DVC_DATE END )),1,3)+ 1911)+RIGHT(DVC_DATE,4))     
-                            END PlanDate,
-                             a.DVC_DATE,a.DVC_STTM,'XRYMDVCF' SourceCode
-                             FROM [10.1.222.182].PXRYDB.SKDBA.XRYMDVCF a
-                             WHERE rtrim(DVC_CHRT) <> ''
-                             AND rtrim(DVC_RQNO) <> ''
-                             AND a.DVC_DATE >=  convert(varchar,dateadd(day,-30,getdate()),112)-19110000
-                            
-                            ) b ON a.MedicalNoteNo = b.DVC_CHRT AND a.ExaRequestNo = b.DVC_RQNO
-                            
-                            select * from #RESTTReservation 
-                                ";
+                            SELECT a.RoomCode RESRoomCode,
+                                   b.RoomCode XRYRoomCode,a.CalendarGroupName,
+                                   a.MedicalNoteNo,a.ExaRequestNo,a.[Start],b.PlanDate ,a.ReservationSourceType,a.SourceCode,
+                                   b.DVC_CHRT,b.DVC_RQNO,b.DVC_DATE,b.DVC_STTM,b.SourceCode XRYSourceCode
+                            INTO #RESTTReservation
+                            FROM 
+                            (
+                              SELECT d.RoomCode,d.CalendarGroupName,a.MedicalNoteNo,a.ExaRequestNo,c.[Start],a.ReservationSourceType,
+                                     'RESTTReservation' SourceCode
+                              FROM HISSCHDB.dbo.RESTTReservation a
+                              INNER JOIN HISSCHDB.dbo.RESTTimeslotRes b ON a.Id = b.ReservationId
+                              INNER JOIN HISSCHDB.dbo.RESTTimeslot c ON b.TimeslotId = c.Id
+                              LEFT JOIN HISSCHDB.dbo.tmpEXAMRoomMapping d ON a.CalendarId = d.CalendarId
+                              WHERE c.[Start] >= convert(date,dateadd(day,-30,getdate()))
+                              --ORDER BY d.RoomCode,c.[Start]
+                            ) a
+                            FULL OUTER JOIN 
+                            (
+                             SELECT SUBSTRING(a.DVC_ROOM,2,2) RoomCode,a.DVC_CHRT,a.DVC_RQNO,
+
+                            CASE WHEN ISNUMERIC(DVC_DATE) = 1 AND ISNUMERIC(DVC_STTM) = 1 
+                            THEN try_convert(datetime,convert(varchar, substring((
+                                            (CASE WHEN LEN(rtrim(DVC_DATE)) = 7 
+                                                  THEN DVC_DATE 
+                             ELSE (CASE WHEN LEFT(DVC_DATE,1) = '0' THEN '1' ELSE '0' END) + DVC_DATE END )) ,1,3)+ 1911)+RIGHT(DVC_DATE,4) +' '+substring(DVC_STTM,1,2)+':'+substring(DVC_STTM,3,2))     
+                             WHEN ISNUMERIC(DVC_DATE) = 1 
+                             THEN try_convert(date,convert(varchar, substring((
+                             (CASE WHEN LEN(rtrim(DVC_DATE)) = 7 
+                                   THEN DVC_DATE 
+                                ELSE (CASE WHEN LEFT(DVC_DATE,1) = '0' THEN '1' ELSE '0' END) + DVC_DATE END )),1,3)+ 1911)+RIGHT(DVC_DATE,4))     
+                             END PlanDate,
+                              a.DVC_DATE,a.DVC_STTM,'XRYMDVCF' SourceCode
+                              FROM [10.1.222.182].PXRYDB.SKDBA.XRYMDVCF a
+                              WHERE rtrim(DVC_CHRT) <> ''
+                              AND rtrim(DVC_RQNO) <> ''
+                              AND a.DVC_DATE >=  convert(varchar,dateadd(day,-30,getdate()),112)-19110000
+
+                             ) b ON a.MedicalNoteNo = b.DVC_CHRT AND a.ExaRequestNo = b.DVC_RQNO
+
+                             select * from #RESTTReservation 
+                                 ";
 
             //Step 1.讀取DB Table List
             IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsetting.json", optional: true, reloadOnChange: true).Build();
@@ -147,13 +148,14 @@ namespace SchedulerDB
                 ExcelWorksheet sheet = package.Workbook.Worksheets[0];
                 int rowIndex = 1;
                 int colIndex = 1;
-                int num = 1;
-                //4.3.1塞資料到某一格
+
+                //3.1塞資料到某一格
                 sheet.Cells[rowIndex, colIndex++].Value = "新檢查室";
                 sheet.Cells[rowIndex, colIndex++].Value = "舊檢查室";
                 sheet.Cells[rowIndex, colIndex++].Value = "檢查室名稱";
                 sheet.Cells[rowIndex, colIndex++].Value = "病歷號";
                 sheet.Cells[rowIndex, colIndex++].Value = "檢查單號";
+                sheet.Cells[rowIndex, colIndex++].Value = "檢查時間";
                 sheet.Cells[rowIndex, colIndex++].Value = "主機病歷號";
                 sheet.Cells[rowIndex, colIndex++].Value = "主機單號";
                 sheet.Cells[rowIndex, colIndex++].Value = "主機排程日";
@@ -161,16 +163,35 @@ namespace SchedulerDB
                 sheet.Cells[rowIndex, colIndex++].Value = "主機檢查碼1";
                 sheet.Cells[rowIndex, colIndex++].Value = "主機檢查碼2";
                 sheet.Cells[rowIndex, colIndex++].Value = "主機檢查碼3";
-
-
                 sheet.Cells[rowIndex, 1, rowIndex, colIndex - 1]
                      .SetQuickStyle(Color.Black, Color.LightPink, ExcelHorizontalAlignment.Center);
 
+                //將對應值放入
+                foreach (var v in migrationTableInfoList)
+                {
+                    if (sheet.ToString() == (v.Start != DateTime.MinValue ? v.Start.ToString("yyyy-MM-dd") : v.PlanDate.ToString("yyyy-MM-dd")))
+                    {
+                        rowIndex++;
+                        colIndex = 1;
+                        sheet.Cells[rowIndex, colIndex++].Value = v.RESRoomCode;
+                        sheet.Cells[rowIndex, colIndex++].Value = v.XRYRoomCode;
+                        sheet.Cells[rowIndex, colIndex++].Value = v.CalendarGroupName;
+                        sheet.Cells[rowIndex, colIndex++].Value = v.MedicalNoteNo;
+                        sheet.Cells[rowIndex, colIndex++].Value = v.ExaRequestNo;
+                        sheet.Cells[rowIndex, colIndex].Value = v.Start;
+                        sheet.Cells[rowIndex, colIndex].Style.Numberformat.Format = DateTimeFormatInfo.CurrentInfo.ShortDatePattern;
+                        sheet.Cells[rowIndex, colIndex++].Style.Numberformat.Format = "yyyy/MM/dd HH:mm:ss";
+                        sheet.Cells[rowIndex, colIndex++].Value = v.DVC_CHRT;
+                        sheet.Cells[rowIndex, colIndex++].Value = v.DVC_RQNO;
+                        sheet.Cells[rowIndex, colIndex++].Value = v.DVC_DATE;
+                        sheet.Cells[rowIndex, colIndex++].Value = v.DVC_STTM;
+                    }
+                }
                 //2020/6/15 上午 12:00:00
                 Console.WriteLine(DateTime.Parse(sheet.ToString()));
                 //2020 - 06 - 15
                 Console.WriteLine(sheet);
-                
+
                 //Autofit
                 int startColumn = sheet.Dimension.Start.Column;
                 int endColumn = sheet.Dimension.End.Column;
